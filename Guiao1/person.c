e8834f0e110ab2bffe6bc5bf36bad9883f26e9f8
Diogo-da-Silva-Rebelo <diogorebelo17@hotmail.com>
2021-03-01 11:04:29 +0000

    Almost finisheddd

--- Guiao1/person.c
+++ Guiao1/person.c
@@ -29,48 +29,60 @@ int novaPessoa(int fd, const char *nome, int idade){
     p1.idade = idade;
     strcpy(p1.nome,nome);
     lseek(fd, 0, SEEK_END); //move o offset para o final do ficheiro
-    write(fd,&p1, sizeof(struct pessoa));
+    write(fd,&p1, sizeof(struct Pessoa));
 }
 
-int atualizaPessoa(int fd, const char *nome, int idade){
-    if(fd == -1) {
+int atualizaPessoa(int fd, const char *nome, int idade) {
+    if (fd == -1) {
         perror("ERROR - open file");
         return 1;
     }
 
-    puts("Modo de atualização 1. ");
+    puts("Modo de atualização 1. Pessoa + idade ");
 
     PESSOA p1;
     int bytes_read;
-    lseek(fd,0,SEEK_SET);
-    while ((bytes_read = read(fd,&p1, sizeof(struct Pessoa))) > 0) {
+    lseek(fd, 0, SEEK_SET);
+    while ((bytes_read = read(fd, &p1, sizeof(struct Pessoa))) > 0) {
         if (!bytes_read) return -1;
-        if (!strcmp(p1.nome,nome)) {
+        if (!strcmp(p1.nome, nome)) {
             p1.idade = idade;
-            strcpy(p1.nome,nome);
-            lseek(fd,-sizeof(struct pessoa),SEEK_CUR);
-            write(fd,&p1, sizeof(struct pessoa));
+            strcpy(p1.nome, nome);
+            lseek(fd, -sizeof(struct Pessoa), SEEK_CUR);
+            write(fd, &p1, sizeof(struct Pessoa));
         }
+    }
 }
 
-int novaIdade(int fd, const char *nome, int idade){
-    puts("Modo de atualização 2. ");
+int novaIdade(int fd, const char *nome, int idade) {
+    puts("Modo de atualização 2. Idade ");
 
-    if(fd == -1) {
+    if (fd == -1) {
         perror("ERROR - open file");
         return 1;
     }
 
     PESSOA p1;
     int bytes_read;
-    lseek(fd,0,SEEK_SET);
-    while ((bytes_read = read(fd,&p1, sizeof(struct Pessoa))) > 0) {
+    lseek(fd, 0, SEEK_SET);
+    while ((bytes_read = read(fd, &p1, sizeof(struct Pessoa))) > 0) {
         if (!bytes_read) return -1;
-        if (!strcmp(p1.nome,nome)) {
+        if (!strcmp(p1.nome, nome)) {
             p1.idade = idade;
-            lseek(fd,-sizeof(struct pessoa),SEEK_CUR);
-            write(fd,&p1, sizeof(struct pessoa));
+            lseek(fd, -sizeof(struct Pessoa), SEEK_CUR);
+            write(fd, &p1, sizeof(struct Pessoa));
         }
+    }
+}
+
+void verFicheiro(int fd, const char *nome, int idade){
+    PESSOA p1; //Cria pessoa
+    lseek(fd,0,SEEK_SET);
+    int byte_read;
+    while((byte_read = read(fd,&p1,sizeof(struct Pessoa))) > 0){
+        printf("\tidade: %d\n\tnome: %s\n",p1.idade,p1.nome);
+        printf("\t--------------------\n");
+    }
 }
 
 int main(int argc, char const *argv[]) {
@@ -86,48 +98,26 @@ int main(int argc, char const *argv[]) {
         return 1;
     }
 
-    int fd = open("registo", O_RDWR | O_CREAT | O_APPEND, 600);
+    int fd = open("registo", O_RDWR | O_CREAT, 0640);
 
-    const char *nome = argv[1];
+    const char *nome = argv[2];
     int idade = atoi(argv[3]);
 
-    switch (*(argv[1])) {
-        case "-i":
-            novaPessoa(fd,nome,idade));
+    switch (*(argv[1] + 1)) {
+        case 'i':
+            novaPessoa(fd, nome, idade);
             break;
-        case "-u":
-            atualizaPessoa(fd,nome,idade);
+        case 'u':
+            atualizaPessoa(fd, nome, idade);
             break;
-        case "-k":
-            novaIdade(fd,nome,idade);
+        case 'k':
+            novaIdade(fd, nome, idade);
             break;
         default:
             puts("Erro: modo inválido");
             break;
     }
-
+    verFicheiro(fd, nome, idade);
     close(fd);
     return 0;
 }
-
-
-    fd = open("id.txt", O_WRONLY | O_CREAT | O_APPEND | S_IRUSR | S_IWUSR, 0640);
-    if (fd == -1) {
-        perror("Erro: não foi possível criar ficheiro de registo");
-        return 1;
-    }
-
-    system("clear");
-    puts("Modo de inserção.");
-
-
-    strcpy(pessoa.nome, argv[2]);
-    pessoa.idade = atoi(argv[3]);
-
-    write(fd, &pessoa, sizeof(struct Pessoa));
-    close(fd);
-    /*   int pos = lseek(fd, sizeof(struct Pessoa), SEEK_CUR);
-       pos = pos / sizeof(struct Pessoa);
-       printf("registo nº %d \n", pos);*/
-}
-//if (!strcmp(argv[2],"-u"))
