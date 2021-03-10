14a912705b1117b64747dc9a6837f9fcd3301bab
Diogo-da-Silva-Rebelo <diogorebelo17@hotmail.com>
2021-03-10 00:27:34 +0000

    Fim do Guião1 (inclusão de scripts).

--- Guiao1/person.c
+++ Guiao1/person.c
@@ -11,22 +11,32 @@
 
 
 int novaPessoa(int fd, const char *nome, int idade){
-    int index;
     if (fd == -1) {
         perror("Não foi possível abrir o ficheiro! ");
         return 1;
     }
 
-    puts("Modo de inserção. ");
+    puts("Modo de inserção.");
+
+    //dá o resgisto em questão
+    struct stat st;
+    fstat(fd, &st);
+    off_t filesize = st.st_size;
+    int position = (int)filesize / sizeof(struct Pessoa);
 
     PESSOA p1;
-    p1.idade = idade;
     strcpy(p1.nome,nome);
-    lseek(fd, 0, SEEK_END); //move o offset para o final do ficheiro
+    p1.idade = idade;
+    lseek(fd,0,SEEK_END);
     write(fd,&p1, sizeof(struct Pessoa));
+    printf("Inserção concluída com sucesso - registo %d\n", position);
+
+    return 0;
 }
 
 int atualizaPessoa(int fd, const char *nome, int idade) {
+    int bytes_read = 0;
+
     if (fd == -1) {
         perror("ERROR - open file");
         return 1;
@@ -35,27 +45,40 @@ int atualizaPessoa(int fd, const char *nome, int idade) {
     puts("Modo de atualização 1. Pessoa + idade ");
 
     PESSOA p1;
-    int bytes_read;
     lseek(fd, 0, SEEK_SET);
-    while ((bytes_read = read(fd, &p1, sizeof(struct Pessoa))) > 0) {
-        if (!bytes_read) return -1;
-        if (!strcmp(p1.nome, nome)) {
-            p1.idade = idade;
-            strcpy(p1.nome, nome);
-            lseek(fd, -sizeof(struct Pessoa), SEEK_CUR);
-            write(fd, &p1, sizeof(struct Pessoa));
-        }
+    while ((bytes_read = read(fd, &p1, sizeof(struct Pessoa))) > 0)
+        if (!strcmp(nome, p1.nome)) {
+        strcpy(p1.nome,nome);
+        p1.idade = idade;
+        lseek(fd, -sizeof(struct Pessoa), SEEK_CUR);
+        write(fd, &p1, sizeof(struct Pessoa));
     }
+    return 0;
 }
 
-int atualizaRegisto(int fd, const char *nome, int idade){
+int atualizaComIndex(int fd, int idade, int index) {
+    if (fd == -1) {
+        perror("Não foi possível abrir o ficheiro! ");
+        return 1;
+    }
 
+    puts("Modo de edição com índice.");
+
+    struct Pessoa p1;
+    lseek(fd, index * sizeof(struct Pessoa), SEEK_SET);
+    if (read(fd, &p1, sizeof(struct Pessoa)) <= 0) return 1;
+    p1.idade = idade;
+    lseek(fd, -(sizeof(struct Pessoa)), SEEK_CUR);
+    write(fd, &p1, sizeof(struct Pessoa));
+    return 0;
 }
+
 void verFicheiro(int fd, const char *nome, int idade){
     PESSOA p1; //Cria pessoa
     lseek(fd,0,SEEK_SET);
     int byte_read;
     while((byte_read = read(fd,&p1,sizeof(struct Pessoa))) > 0){
+        printf("\t--------------------\n");
         printf("\tidade: %d\n\tnome: %s\n",p1.idade,p1.nome);
         printf("\t--------------------\n");
     }
@@ -67,6 +90,7 @@ int main(int argc, char const *argv[]) {
      * -> 2 comandos que permitam inserir e alterar dados de pessoas, num determinado ficheiro.
      * -> Criar um ficheiro de leitura;
      * -> Programar a sua alteração e inserção;
+     * -> Os registos começam em 0!!!
      * -> Não esquecer de utilizar o stdin e stdout, pois é a partir daí que os comandos serão utilizados! */
 
     if (argc != 4) {
@@ -74,20 +98,22 @@ int main(int argc, char const *argv[]) {
         return 1;
     }
 
-    int fd = open("registo", O_RDWR | O_CREAT | O_APPEND, 0640);
+    int fd = open("registo", O_CREAT | O_RDWR , 0640);
 
     const char *nome = argv[2];
     int idade = atoi(argv[3]);
 
     switch (*(argv[1] + 1)) {
+
         case 'i':
             novaPessoa(fd, nome, idade);
             break;
+
         case 'u':
+            if(isdigit(*argv[2])) atualizaComIndex(fd, idade, atoi(argv[2]));
             atualizaPessoa(fd, nome, idade);
             break;
-        case 'r':
-            atualizaRegisto(fd, nome, idade);
+
         default:
             puts("Erro: modo inválido");
             break;
