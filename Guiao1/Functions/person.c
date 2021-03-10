516e6da529654a5c5989e85adf6187f3d8840342
Diogo-da-Silva-Rebelo <diogorebelo17@hotmail.com>
2021-03-10 09:53:53 +0000

    Inclusão de algumas notas sobre os Guiões 1 e 2.

--- Guiao1/Functions/person.c
+++ Guiao1/Functions/person.c
@@ -18,7 +18,22 @@ int novaPessoa(int fd, const char *nome, int idade){
 
     puts("Modo de inserção.");
 
-    //dá o resgisto em questão
+    /* dá o resgisto em questão. Alguns aspetos importantes sobre a struct stat:
+     * -> int fstat(int fd, struct stat *statbuf); fstat() is identical to stat(), except that the file about which
+     * information is to be retrieved is specified by the file descriptor fd.
+     * -> struct stat {
+    dev_t     st_dev;      ID of device containing file
+    ino_t     st_ino;         Inode number
+    mode_t    st_mode;        File type and mode
+    nlink_t   st_nlink;       Number of hard links
+    uid_t     st_uid;         User ID of owner
+    gid_t     st_gid;         Group ID of owner
+    dev_t     st_rdev;        Device ID (if special file)
+    off_t  -> st_size;        Total size, in bytes
+    blksize_t st_blksize;     Block size for filesystem I/O
+    blkcnt_t  st_blocks;      Number of 512B blocks allocated
+     */
+
     struct stat st;
     fstat(fd, &st);
     off_t filesize = st.st_size;
@@ -57,6 +72,7 @@ int atualizaPessoa(int fd, const char *nome, int idade) {
 }
 
 int atualizaComIndex(int fd, int idade, int index) {
+    //index é o registo que queremos alterar!
     if (fd == -1) {
         perror("Não foi possível abrir o ficheiro! ");
         return 1;
