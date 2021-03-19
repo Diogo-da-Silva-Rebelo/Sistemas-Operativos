932621ed8e2ffda0258123dc66b4db931ee2bd1d
Diogo-da-Silva-Rebelo <diogorebelo17@hotmail.com>
2021-03-19 02:14:46 +0000

    Continuação da resolução dos exercícios.

--- Guiao2/ex3.c
+++ Guiao2/ex3.c
@@ -1,20 +1,24 @@
-#include <unistd.h>
-#include <stdio.h>
-#include <sys/wait.h>
-
+#include "process.h"
+/* Implemente um programa que crie dez processos filhos que deverão executar sequencialmente. Para este
+efeito, os filhos podem imprimir o seu PID e o do seu pai, e finalmente, terminarem a sua execução com
+um valor de saı́da igual ao seu número de ordem (e.g.: primeiro filho criado termina com o valor 1). O
+pai deverá imprimir o código de saı́da de cada um dos seus filhos. */
 int main(int argc, char* argv[]){
     int i;
     int status;
 
-    for (i=0; i<3; i++){
-        int pid = fork();
-        if(pid==0){
-            printf("%d\n", i);
+    for (i=1; i<11; i++){
+        pid_t pid;
+        if((pid = fork()) == 0) {
+            pid_t child_pid = getpid();
+            pid_t father_pid = getppid();
+            printf("[filho] O PID do processo filho é %d.\nO pid do processo pai é %d.\n", child_pid, father_pid);
+            printf("%d\n\n", i);
             _exit(i);
         }
         else {
-            pid_t terminated_pid = wait(&status);
-            printf("[pai] process %d exited. exit code: %d\n", terminated_pid, WEXITSTATUS(status));
+            int terminated_pid = wait(&status);
+            printf("[pai] process %d exited. exit code: %d.\n\n", terminated_pid, WEXITSTATUS(status));
         }
     }
     return 0;
--- Guiao2/ex4.c
+++ Guiao2/ex4.c
@@ -1,24 +1,19 @@
-#include <unistd.h>
-#include <stdio.h>
-#include <sys/wait.h>
+#include "process.h"
 
-int main(int argc, char* argv[]){
+/* Implemente um programa que crie dez processos filhos que deverão executar em concorrência. O pai
+deverá esperar pelo fim da execução de todos os seus filhos, imprimindo os respectivos códigos de saı́da. */
+int main(int argc, char* argv[]) {
     int i;
     int status;
 
-    for (i=0; i<3; i++) {
-        int pid = fork();
-        if (pid == 0) {
-            printf("%d\n", i);
+    for (i = 1; i < 11; i++) {
+        pid_t pid;
+        if ((pid = fork()) == 0) {
+            //printf("%d\n", i);
             _exit(i);
         }
     }
-
-    for (i=0; i<4; i++){
-        pid_t terminated_pid = wait(&status);
-        printf("[pai] process %d exited. exit code: %d\n", terminated_pid, WEXITSTATUS(status));
-    }
-
+    pid_t terminated_pid = wait(&status);
+    printf("[pai] process %d exited. exit code: %d\n", terminated_pid, WEXITSTATUS(status));
     return 0;
 }
-
