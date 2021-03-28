af1f550b48c00d4e4a18514659be9f02f92b93a6
Diogo-da-Silva-Rebelo <diogorebelo17@hotmail.com>
2021-03-28 02:33:14 +0100

    Ex3 started.

---
+++ Guiao3/ex3.c
@@ -0,0 +1,8 @@
+#include "exec.h"
+
+/* Implemente um programa que imprima a lista de argumentos recebidos na sua linha de comando. */
+
+int main(int argc, char const *argv[]) {
+
+    return 0;
+}
