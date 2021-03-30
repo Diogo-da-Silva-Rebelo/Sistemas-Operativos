87b1b302499686b83aeebd92e252b739682b2dd4
Diogo-da-Silva-Rebelo <diogorebelo17@hotmail.com>
2021-03-30 01:07:59 +0100

    Ex5 half done.

---
+++ Guiao3/ex5.c
@@ -0,0 +1,15 @@
+#include "exec.h"
+
+/* Implemente um programa que execute concorrentemente uma lista de executáveis especificados como
+argumentos da linha de comando. Considere os executáveis sem quaisquer argumentos próprios. O
+programa deverá esperar pelo fim da execução de todos processos por si criados. */
+
+int main(int argc, const char* argv[]) {
+    for(size_t i = 1; i < argc; i++)
+        if(fork() == 0)
+            execlp(argv[i], argv[i], NULL);
+
+    for(size_t i = 1; i < argc; i++)
+        wait(NULL);
+    return 0;
+}
