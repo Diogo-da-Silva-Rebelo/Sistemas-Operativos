a8cc1cfc20d3817b70a67677289547f7babe2c7d
Diogo-da-Silva-Rebelo <diogorebelo17@hotmail.com>
2021-05-24 00:02:40 +0100

    Improves

--- Guiao2/Functions/ex2.c
+++ Guiao2/Functions/ex2.c
@@ -13,14 +13,19 @@ int main(int argc, char const *argv[]) {
         pid_t childp_pid = getppid();
         printf("O PID do filho é %d.\nO PID do respetivo pai é %d.\n", child_pid, childp_pid);
         _exit(0);
-    }
-    else {
+    } else {
         wait(&status);
+        if (pid != -1 && WIFEXITED(status)) {
+            puts("Processo filho terminou normalmente.\n");
+            int const exit_status = WEXITSTATUS(status);
+            printf("Exit status do processor filho: %d\n", exit_status);
+        }
+
         pid_t parent_pid = getpid();
         pid_t parentp_pid = getppid();
         pid_t parentChild_pid = pid;
-        printf("O PID do pai é %d.\nO PID do pai do pai é %d.\nO PID do filho do pai inicial é %d.\n", parent_pid, parentp_pid, parentChild_pid);
-
+        printf("O PID do pai é %d.\nO PID do pai do pai é %d.\nO PID do filho do pai inicial é %d.\n", parent_pid,
+               parentp_pid, parentChild_pid);
     }
     return 0;
 }
